fc4c35795db6802f7991edb6234b5785a587c286
Brendan Wade Weimer <bweimer@RRC.CA>
2016-12-21 15:38:44 -0600

    Maybe working laptop not able to handle it

---
+++ app/src/main/java/com/example/bweimer/firebasetest/Config.java
@@ -0,0 +1,9 @@
+package com.example.bweimer.firebasetest;
+
+/**
+ * Created by bweimer on 12/21/2016.
+ */
+
+public class Config {
+    public static final String FIREBASE_URL = "https://fir-test-84804.firebaseio.com/";
+}
